87c3c3c7217de879b7f5f3a9720f77e9a85b240d
Lukas <krouza27@gmail.com>
2022-08-08 13:01:33 +0200

    first inicailization

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {useState,useEffect} from 'react';
+//https://pokeapi.co/api/v2/pokemon
+import './App.css';
+import axios from 'axios';
+
+
+function App() {
+  
+  return (
+    <div className='app-container'>
+      <h1>Hi </h1>
+    </div>
+  );
+}
+
+export default App;
